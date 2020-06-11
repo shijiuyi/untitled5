4aad91437b57b6fe479ef682e492ec6dd0e626a7
shijiuyi <shijiuyi@socmap.net>
2020-06-11 18:11:33 +0800

    xml格式转换为json格式文本

--- xml_trans_json.py
+++ xml_trans_json.py
@@ -17,16 +17,29 @@ def load_json(xml_path):
     xml_file = open(xml_path, 'r')
     xml_str = xml_file.read()
     json_dict = xmltodict.parse(xml_str)
-    json_str = json.dumps(json_dict, indent=2)
+    # indent实现换行和空格的间隔长度
+    # ensure_ascii=False实现让中文写入的时候保持为中文
+    json_str = json.dumps(json_dict, indent=2, ensure_ascii=False)
     # print(json_str)
     return json_str
 
 
 json_1 = load_json("/home/shijiuyi/下载/2020-04-27_2020-05-03.xml")
 json_1 = json_1.encode()
-file_path = "/home/shijiuyi/下载/trans.json"
+# json_1 = str(bytes, 'utf-8')
+# print(json_1)
+
+file_path = "/home/shijiuyi/桌面/test_file/json/trans.json"
+if not os.path.exists(file_path):
+    os.mknod(file_path)
+
 with open(file_path, 'wb') as f:
     f.write(json_1)
+
+# json_path = "/home/shijiuyi/桌面/test_file/trans.json"
+# with open(file_path) as f:
+#     res = f.read()
+#     f.write(res)
 
 
 # if __name__ == '__main__':
